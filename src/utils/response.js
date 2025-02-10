ab71e2b561f5355c0113ae5124f69eb0f71c6ece
Luxcus Qing <ai2845612055@outlook.com>
2025-02-11 00:17:29 +0800

    Update response.js

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,1 +1,19 @@
+export function createJSONResponse(data, status = 200, headers = {}) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers
+    }
+  });
+}
 
+export function createHTMLResponse(html, headers = {}) {
+  return new Response(html, {
+    headers: {
+      'Content-Type': 'text/html',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      ...headers
+    }
+  });
+}
