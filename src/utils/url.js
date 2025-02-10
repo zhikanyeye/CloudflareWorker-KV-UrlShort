41ec2431f582a9f141f83900c378cd0916f2b440
Luxcus Qing <ai2845612055@outlook.com>
2025-02-11 00:16:13 +0800

    Update url.js

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,1 +1,23 @@
+import { CONFIG } from '../config/constants';
 
+export function validateURL(url) {
+  try {
+    const urlObject = new URL(url);
+    if (!urlObject.protocol.match(/^https?:$/)) {
+      throw new Error(CONFIG.ERRORS.INVALID_URL);
+    }
+    return urlObject.toString();
+  } catch {
+    throw new Error(CONFIG.ERRORS.INVALID_URL);
+  }
+}
+
+export function validateSlug(slug) {
+  if (slug.length < CONFIG.MIN_CUSTOM_SLUG_LENGTH) {
+    throw new Error(CONFIG.ERRORS.SLUG_TOO_SHORT);
+  }
+  
+  if (!CONFIG.REGEX.SLUG.test(slug)) {
+    throw new Error(CONFIG.ERRORS.INVALID_SLUG);
+  }
+}
