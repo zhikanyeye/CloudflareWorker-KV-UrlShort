a3dccdbe40c285160581831fa57684a49502832d
Luxcus Qing <ai2845612055@outlook.com>
2025-02-11 00:16:55 +0800

    Update slug.js

--- src/utils/slug.js
+++ src/utils/slug.js
@@ -1,1 +1,9 @@
+import { CONFIG } from '../config/constants';
 
+export function generateSlug(length = CONFIG.SLUG_LENGTH) {
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  return Array.from(
+    { length }, 
+    () => chars[crypto.getRandomValues(new Uint32Array(1))[0] % chars.length]
+  ).join("");
+}
